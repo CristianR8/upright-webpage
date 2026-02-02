315053580bc91c1c28bb556ceb96339fa1253668
CristianR8 <cristiancamiloreyrueda@gmail.com>
2026-02-02 15:55:38 -0500

    ana exit

--- src/components/UI/Featured/constants.ts
+++ src/components/UI/Featured/constants.ts
@@ -8,7 +8,6 @@ import a5 from '../../../../public/images/allies/5.png';
 import a6 from '../../../../public/images/allies/6.png';
 import a7 from '../../../../public/images/allies/7.png';
 import a8 from '../../../../public/images/allies/8.png';
-import a9 from '../../../../public/images/allies/9.png';
 
-export const alliesImages: StaticImageData[] = [a1, a2, a3, a4, a5, a6, a7, a8, a9];
+export const alliesImages: StaticImageData[] = [a1, a2, a3, a4, a5, a6, a7, a8];
 
